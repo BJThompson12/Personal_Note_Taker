4b2ef0c79ab92893111a94269f90fec8022b6bad
Brandon Thompson <brandonjoel.thompson@gmail.com>
2023-03-12 17:57:40 -0400

    added express to json and url

--- routes/routes.js
+++ routes/routes.js
@@ -1,14 +1,14 @@
 // require the router to be used
 const router = require('express').Router();
-// require express path to add the file string
+// require  path to add the file string
 const path = require('path');
+// require express
 const express = require('express');
 // require path to 
 const app = express();
 
 // get from the home page to send the notes page
 router.get('/notes', (req, res) => {
-  console.log(req);
   res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
--- server.js
+++ server.js
@@ -9,6 +9,14 @@ app.use('/', routes);
 // To serve static files such as images, CSS files, and JavaScript files, use the express.static built-in middleware function in Express.
 app.use(express.static('public'));
 
+// You can get POST body data using express.json() or express.urlencoded() to support JSON and URL-encoded bodies
+
+// parse TO JSON
+app.use(express.json());
+// parse for web
+app.use(express.urlencoded({extended: true}))
+
+
 // set listening on the port
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
