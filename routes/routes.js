474183cf16b9bcf405de07334e31a0bae9b4f53d
Brandon Thompson <brandonjoel.thompson@gmail.com>
2023-03-12 16:42:40 -0400

    set up staic express to display html pages

--- routes/routes.js
+++ routes/routes.js
@@ -3,10 +3,14 @@ const router = require('express').Router();
 // require express path to add the file string
 const path = require('path');
 const express = require('express');
-// rewuire path to 
+// require path to 
 const app = express();
 
 // get from the home page to send the notes page
 router.get('/notes', (req, res) => {
+  console.log(req);
   res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
+
+//export the router
+module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
-const PORT = process.env.PORT || 3001;
 const app = express();
+const PORT = process.env.PORT || 3001;
 const routes = require("./routes/routes.js");
+
+
+// //set app to use routes
+app.use('/', routes);
+// To serve static files such as images, CSS files, and JavaScript files, use the express.static built-in middleware function in Express.
+app.use(express.static('public'));
 
 // set listening on the port
 app.listen(PORT, () => {
