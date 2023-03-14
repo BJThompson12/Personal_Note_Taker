dc7bd1cbc892bdacd43691eee5ae614075b23e9e
Brandon Thompson <brandonjoel.thompson@gmail.com>
2023-03-14 13:06:43 -0400

    installed npm uuid and added get route

--- routes/APIroutes.js
+++ routes/APIroutes.js
@@ -1,18 +1,20 @@
 // require the router to be used
 const router = require('express').Router();
-// // require  path to add the file string
+// require  path to add the file string
  const path = require('path');
+ // require fs to read and write
+ const fs = require('fs')
+// require database to read and write and send data to
+const notesDatabase = require('../db/db.json')
+// require the ID generator
+// uuidv4() to use
+const { v4: uuidv4 } = require('uuid');
 
-// // require path to 
-// const app = express();
-// // require database to send data to
-// const notesDatabase = require('../db/db.json')
 
-
-//get the information from the posted note
+//get the information from the database
 router.get('/api/notes', (req, res) => {
-  // console.log(res.json());
-  // console.log('i got the data');
+  res.sendFile(path.join(__dirname, "../db/db.json"));
+  });
 
 //call out the store and then define the date read and write the data
 //create method to get the notes 
@@ -21,12 +23,10 @@ router.get('/api/notes', (req, res) => {
 // needd to requre util and fs - create the methods in your varioables - always need a return in the methos and can be used
 
 // create a variable to require in the JSON
-// read the db KSON file 
+// read the db JSON file 
 //write the db JSON
 
   
-  // need to send the data to the database in json format
-});
 router.post('/api/notes', (req, res) => {
   console.log(req.body);
   console.log('i got the data');
