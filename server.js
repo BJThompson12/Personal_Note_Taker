665c2470e198b3e0c5b0195586e92a3555e86810
Brandon Thompson <brandonjoel.thompson@gmail.com>
2023-03-16 02:07:27 -0400

    bug

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 
-const router = require('./routes/apiRoutes.js')
+const router = require('./routes/apiRoutes')
 const app = express();
 
 const PORT = process.env.PORT || 3000;
