0c1e50462a0595d221388de14a03fb015af03b02
Brandon Thompson <brandonjoel.thompson@gmail.com>
2023-03-15 22:50:23 -0400

    moved routers

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const apiRouter = require("./routes/apiRoutes");
-const htmlRouter = require("./routes/htmlRoutes");
+// const apiRouter = require("./routes/apiRoutes");
+// const htmlRouter = require("./routes/htmlRoutes");
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -13,10 +14,20 @@ app.use(express.urlencoded({extended: true}))
 // To serve static files such as images, CSS files, and JavaScript files, use the express.static built-in middleware function in Express.
 app.use(express.static('public'));
 
+ // router to go back to home page
+ app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+// get from the home page to send the notes page
+app.get('/notes', (req, res) => {
+ res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
 // set app to use html route
-app.use('/', htmlRouter);
+//app.use('/', htmlRouter);
 //set app to use route
-app.use('/api', apiRouter);
+//app.use('/api', apiRouter);
 
 // set listening on the port
 
