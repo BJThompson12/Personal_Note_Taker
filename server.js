1e1783b5f6a66afcff4ff2045991b20723994094
Brandon Thompson <brandonjoel.thompson@gmail.com>
2023-03-16 00:27:32 -0400

    bugs

--- server.js
+++ server.js
@@ -5,9 +5,11 @@ const html2Router = require('./routes/html2Route');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+
 // parse for web
 app.use(express.urlencoded({extended: true}))
 app.use(express.json());
+app.use(express.static('public'));
 
 // middleware has to be in order bc it reads top to bottom of file
 // parse TO JSON
@@ -16,10 +18,10 @@ app.use(express.json());
 
 //set app to use route
 app.use('/api', apiRouter);
-app.use(express.static('public'));
-app.use('/', htmlRouter)
 // set app to use html route
 app.use('/', html2Router)
+app.use('/', htmlRouter)
+
 // set listening on the port
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
