36759373399f5b3299828b24b1b13bd8788c8940
Brandon Thompson <brandonjoel.thompson@gmail.com>
2023-03-15 23:54:47 -0400

    bugs

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const htmlRouter = require('./routes/htmlRoutes');
 const apiRouter = require('./routes/apiRoutes')
+const htmlRouter = require('./routes/htmlRoutes');
 
 // middleware has to be in order bc it reads top to bottom of file
 // parse TO JSON
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 
 // To serve static files such as images, CSS files, and JavaScript files, use the express.static built-in middleware function in Express.
-app.use(express.static('public'));
+
 
  // router to go back to home page
 //  app.get('/', (req, res) => {
@@ -26,7 +26,7 @@ app.use(express.static('public'));
 
 //set app to use route
 app.use('/api', apiRouter);
-
+app.use(express.static('public'));
 app.use('/', htmlRouter)
 // set app to use html route
 
