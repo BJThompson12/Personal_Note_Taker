6cd32924080b54a45c5ae8723285b10a73a33cd9
Brandon Thompson <brandonjoel.thompson@gmail.com>
2023-03-16 01:35:34 -0400

    file fixes

--- server.js
+++ server.js
@@ -1,31 +1,28 @@
 const express = require('express');
-// const htmlRouter = require('./routes/htmlRoutes');
-//const html2Router = require('./routes/html2Route');
 const path = require('path');
+// const htmlRouter = require('./routes/htmlRoutes');
 const apiRouter = require('./routes/apiRoutes')
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// To serve static files such as images, CSS files, and JavaScript files, use the express.static built-in middleware function in Express.
+app.use(express.static('public'));
 
 // parse for web
 app.use(express.urlencoded({extended: true}))
 app.use(express.json());
-// To serve static files such as images, CSS files, and JavaScript files, use the express.static built-in middleware function in Express.
-app.use(express.static('public'));
+
+// router to go back to home page
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+ });
 
  // get from the home page to send the notes page
  app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/notes.html'))
-  console.log(__filename);
-  test = __dirname, './public/notes.html'
-  console.log(test);
+  res.sendFile(path.join(__dirname, 'public/notes.html'))
  });
-// router to go back to home page
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
- });
-
-
-
+ 
 //set app to use route
 app.use('/api', apiRouter);
 // set app to use html route
