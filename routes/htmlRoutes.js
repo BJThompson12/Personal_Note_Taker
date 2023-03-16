23acb903d2ca334a831fa031c2f2fe48977647db
Brandon Thompson <brandonjoel.thompson@gmail.com>
2023-03-15 23:29:41 -0400

    added dot

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -7,7 +7,7 @@ const htmlRouter = express.Router()
 
  // router to go back to home page
  htmlRouter.get('/', (req, res) => {
-   res.sendFile(path.join(__dirname, './public/index.html'));
+   res.sendFile(path.join(__dirname, '../public/index.html'));
  });
  
 // get from the home page to send the notes page
