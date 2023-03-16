70ed6df220c3e8f3f135526e1917eb9a3d9ded98
Brandon Thompson <brandonjoel.thompson@gmail.com>
2023-03-15 23:50:39 -0400

    bugs

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,14 +5,15 @@ const htmlRouter = express.Router()
 // // require  path to add the file string
  const path = require('path');
 
+ 
  // get from the home page to send the notes page
  htmlRouter.get('/notes', (req, res) => {
    res.sendFile(path.join(__dirname, '../public/notes.html'));
- });
+  });
  // router to go back to home page
- htmlRouter.get('*', (req, res) => {
+ htmlRouter.get('*', (req, res, next) => {
    res.sendFile(path.join(__dirname, '../public/index.html'));
- });
- 
+   next()
+  });
 
 module.exports = htmlRouter;
