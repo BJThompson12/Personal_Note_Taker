b07ea13c5e509fdb1123e9f1ed751599c4e5d0a0
Brandon Thompson <brandonjoel.thompson@gmail.com>
2023-03-16 00:25:46 -0400

    bugs

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require('express');
+const apiRouter = require('./routes/apiRoutes')
+const htmlRouter = require('./routes/htmlRoutes');
+const html2Router = require('./routes/html2Route');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const apiRouter = require('./routes/apiRoutes')
-const htmlRouter = require('./routes/htmlRoutes');
+// parse for web
+app.use(express.urlencoded({extended: true}))
+app.use(express.json());
 
 // middleware has to be in order bc it reads top to bottom of file
 // parse TO JSON
-app.use(express.json());
-// parse for web
-app.use(express.urlencoded({extended: true}))
 
 // To serve static files such as images, CSS files, and JavaScript files, use the express.static built-in middleware function in Express.
 
@@ -18,7 +19,7 @@ app.use('/api', apiRouter);
 app.use(express.static('public'));
 app.use('/', htmlRouter)
 // set app to use html route
-
+app.use('/', html2Router)
 // set listening on the port
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
